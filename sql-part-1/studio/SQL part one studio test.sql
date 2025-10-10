25abd936cc28f0bc5efa694f010b62c7387b6218
Megan Murphy <meg.murphy9134@gmail.com>
2025-10-10 15:41:41 -0500

    completed part 2 exercises

--- sql-part-1/studio/SQL part one studio test.sql
+++ sql-part-1/studio/SQL part one studio test.sql
@@ -1,3 +1,6 @@
+-- Part One --
+-- STUDIO --
+
 -- USE booksdb
 -- SELECT TOP 100 book_id, authors, title, average_rating AS book_rating
 -- FROM dbo.books
@@ -50,9 +53,43 @@
 -- FROM dbo.books
 -- WHERE original_publication_year = 2005;
 
-USE BooksDB
-SELECT title, authors, average_rating
-FROM dbo.books
-WHERE authors = 'J.K. Rowling'
-ORDER BY authors, average_rating DESC;
+-- USE BooksDB
+-- SELECT title, authors, average_rating
+-- FROM dbo.books
+-- WHERE authors = 'J.K. Rowling'
+-- ORDER BY authors, average_rating DESC;
 
+-- PART TWO --
+-- ASSIGNMENT --
+
+-- USE RideShareDB
+-- SELECT RIGHT(Base_Name, 3)
+-- FROM dbo.other_FHV_services_jan_aug_2015;
+
+-- USE RideShareDB
+-- SELECT CHARINDEX(' ', Number_of_Trips)
+-- FROM dbo.other_FHV_services_jan_aug_2015;
+
+-- USE RideShareDB
+-- SELECT CHARINDEX(' ', Pick_Up_Date)
+-- FROM dbo.other_FHV_services_jan_aug_2015;
+
+-- USE RideShareDB
+-- SELECT DATENAME(month, Pick_Up_Date)
+-- FROM dbo.other_FHV_services_jan_aug_2015;
+
+-- USE RideShareDB
+-- SELECT FORMAT(Pick_Up_Date, 'dd/MM')
+-- FROM dbo.other_FHV_services_jan_aug_2015;
+
+-- USE RideShareDB
+-- SELECT MAX(start_lng)
+-- FROM lyft;
+
+-- USE RideShareDB
+-- SELECT MAX(start_lat)
+-- FROM lyft;
+
+-- USE RideShareDB
+-- SELECT CONCAT(Base_Number, ': ', Base_Name)
+-- FROM dbo.other_FHV_services_jan_aug_2015;
