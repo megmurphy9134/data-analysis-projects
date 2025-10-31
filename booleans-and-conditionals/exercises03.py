e93dc930f721298c8fa52c08f13b84084468b860
Megan Murphy <meg.murphy9134@gmail.com>
2025-10-31 15:21:30 -0500

    loops assignments complete

--- booleans-and-conditionals/exercises03.py
+++ booleans-and-conditionals/exercises03.py
@@ -28,30 +28,21 @@ engine_temperature = 1200
 # f) Otherwise, print "Fuel and engine status pending..." 
 
 # Code 5a - 5f here:
-#AND
-if fuel_level > 20000:
-    if engine_temperature <= 2500:
-        print("Full tank. Engines good.")
-#AND
-elif fuel_level > 10000:
-    if engine_temperature <= 2500:
-        print("Fuel level above 50%. Engines good.")
-#AND
-elif fuel_level > 5000:
-    if engine_temperature <= 2500:
-        print("Fuel level above 25%. Engines good.")
-#OR
-elif fuel_level <= 5000:
-    if engine_temperature > 2500:
-        print("Check fuel level. Engines running hot.")
-#OR
-elif fuel_level <= 1000:
-    if engine_temperature > 3500:
-        if engine_indicator_light == 'red blinking':
-            print("ENGINE FAILURE IMMINENT")
+
+if (fuel_level < 1000 or engine_temperature > 3500 or engine_indicator_light == "red blinking"):
+    print("ENGINE FAILURE IMMINENT")
+elif (fuel_level <= 5000 or engine_temperature > 2500):
+    print("Check fuel level. Engines running hot.")
+elif (fuel_level > 20000 and engine_temperature <= 2500):
+    print("Full tank. Engines good.")
+elif (fuel_level > 10000 and engine_temperature <= 2500):
+    print("Fuel level above 50%. Engines good.")
+elif (fuel_level > 5000 and engine_temperature <= 2500):
+    print("Fuel level above 25%. Engines good.")
 else:
-  print("Fuel and engine status pending...")
-## NOT COMPLETE - REVISIT
+    print("Fuel and engine status pending...")
+
+# Output: Engine Failure Imminent
 
 # 6) a) Create the variable command_override, and set it to be true or false.
 # If command_override is false, then the shuttle should only launch if the fuel and engine check are OK.
@@ -59,9 +50,13 @@ else:
 
 command_override = False
 
+if command_override == False:
+    print('Check fuel and engine before engine launch.')
+else:
+    print('Shuttle will launch.')
+
 # 6) b) Code the following if/else check:
-if fuel_level > 20000 AND engine_indicator_light != 'red blinking' OR command_override == True:
-    print("Cleared to launch")
-
-# If fuel_level is above 20000 AND engine_indicator_light is NOT red blinking OR command_override is true print "Cleared to launch!" Else print "Launch scrubbed!"
-
+if (fuel_level > 20000 and engine_indicator_light != 'red blinking' or command_override == True):
+   print("Cleared to launch")
+else:
+    print('Launch scrubbed!')
--- loops/exercises/while-loop-exercises.py
+++ loops/exercises/while-loop-exercises.py
@@ -1,25 +1,39 @@
-# Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
-
-
-
-
+# Define three variables for the LaunchCode shuttle - 
+# one for the starting fuel level, 
+# another for the number of astronauts aboard,
+# and the third for the altitude the shuttle reaches.
 
 # Exercise #1: Construct while loops to do the following:
-  # a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. 
+# a. Query the user for the starting fuel level.
+# Validate that the user enters a positive integer value
+# greater than 5000 but less than 30000. 
+fuel_lvl = 0
 
-
-
-
+while (fuel_lvl <= 5000 or fuel_lvl >= 30000):
+  fuel_lvl = int(input('Starting fuel level:'))
+  if (fuel_lvl <= 5000 or fuel_lvl >= 30000):
+    print('Invalid number. Fuel level is between 5000 and 30000.')
 
 # b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-  
-  
-  
+count_astronauts = 0
+
+while count_astronauts:
+  count_astronauts = int(input('Number of astronauts:'))
+if (count_astronauts < 1 or count_astronauts > 7):
+  print('Invalid response. Please enter a value 1 - 7.')
   
 # c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
+shuttle_altitude = 0
 
-
+while fuel_lvl - 100 * count_astronauts >= 0:
+  shuttle_altitude += 50
+  fuel_lvl -= 100 * count_astronauts
 
 # Exercise #2: Print the result with the phrase, The shuttle gained an altitude of ___ km and has ___ kg of fuel left. Fill in the blanks with the altitude and fuel level values.
 
 # If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”
+if shuttle_altitude >= 2000:
+  ending = 'Orbit achieved!'
+else:
+  ending = 'Failed to reach orbit.'
+print('The shuttle gained an altitude of', shuttle_altitude, 'km and has', fuel_lvl, 'kg of fuel left.', ending)
