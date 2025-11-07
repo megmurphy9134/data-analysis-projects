3a741484aadb5dd568f52487dbae7324be85cfbd
Megan Murphy <meg.murphy9134@gmail.com>
2025-11-06 20:26:33 -0600

    strings and lists studio upload

--- collections/studio/multi-dimensional-lists.py
+++ collections/studio/multi-dimensional-lists.py
@@ -3,20 +3,31 @@ equipment = "space suits,jet packs,tool belts,thermal detonators"
 pets = "parrots,cats,moose,alien eggs"
 sleep_aids = "blankets,pillows,eyepatches,alarm clocks"
 
-# a) Use split to convert the strings into four cabinet lists. Alphabetize the contents of each cabinet.
+# a) Use split to convert the strings into four cabinet lists.
+#  Alphabetize the contents of each cabinet.
 
-
+food_list = sorted(food.split(','))
+equipment_list = sorted(equipment.split(','))
+pets_list = sorted(pets.split(','))
+sleep_aids = sorted(sleep_aids.split(','))
 
 # b) Initialize a cargo_hold list and add the cabinet lists to it. Print cargo_hold to verify its structure.
+cargoHold = []
 
+cargoHold.append(food_list)
+cargoHold.append(equipment_list)
+cargoHold.append(pets_list)
+cargoHold.append(sleep_aids)
 
+print(cargoHold)
 
 # c) Query the user to select a cabinet (0 - 3) in the cargo_hold.
-
-
+cabinet = int(input("Select a cabinet between 0 and 3: "))
 
 # d) Use bracket notation and format to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-
-
+#BROKEN
+# for cabinet in cargoHold:
+#   print(f"The following items are in cabinet {cabinet}: {cargoHold[cabinet]}")
+    
 
 # e) Modify the code to query the user for BOTH a cabinet in cargo_hold AND a particular item. Use the in method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
--- functions/exercises/functions-exercises.py
+++ functions/exercises/functions-exercises.py
@@ -1,40 +1,72 @@
 # Part 1 A -- Make a Line
+def make_line(size):
+    line = ''
+    for i in range(size):
+        line += '#'
+    return line
 
-
+print(make_line(5))
 
 # Part 1 B -- Make a Square
 # create a function using your make_line function to code a square
-
-
-
-
+def make_square(size):
+    side = ''
+    for i in range(size):
+        side += make_line(size) + '\n'
+    return side
+    
+print(make_square(5))
 
 # Part 1 C -- Make a Rectangle
-
-
-
+def make_rectangle(width, height):
+    rectangle = ''
+    for i in range(height):
+        rectangle += make_line(width) + '\n'
+    return rectangle
+    
+print(make_rectangle(5,3))
 
 
 # Part 2 A -- Make a Stairs
+def make_downward_stairs(height):
+    stair = ''
+    for i in range(height):
+        stair += make_line(i + 1) + '\n'
+    return stair
 
-
-
-
+print(make_downward_stairs(5))
 
 # Part 2 B -- Make Space-Line 
+def make_space_line(numSpaces, numChars):
+    space = ''
+    hash = ''
+    for i in range(numSpaces):
+        space += '_'
+    for i in range(numChars):
+        hash += '#'
+    return print(space + hash + space)
 
-
-
-
+make_space_line(3, 5)
 
 # Part 2 C -- Make Isosceles Triangle
+def make_isosceles_triangle(height):
+    triangle = ''
+    for i in range(height):
+        triangle += make_space_line((height - i - 1), (2 * i + 1)) + '\n'
+    return triangle
 
-
-
-
+print(make_isosceles_triangle(5))
 
 # Part 3 -- Make a Diamond
+def make_diamond(height):
+    diamond = ''
+    triangle = make_isosceles_triangle(height)
+    diamond += triangle[:-1]
+    for i in range(len(triangle) - 1, -1, -1):
+        diamond += triangle[i]
+    return diamond
 
+print(make_diamond(5))
 
 
 
